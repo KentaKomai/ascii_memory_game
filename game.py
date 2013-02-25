f11f8f32a31475e6426886e7a944e8e4624fb344
Kenta Komai <kaede06152000@gmail.com>
2013-02-25 12:14:58 +0900

    implements mod- change

--- game.py
+++ game.py
@@ -10,21 +10,35 @@ if __name__ == "__main__":
 	print("url: http://tamago.servehttp.com")
 	print("")
 
+	mode = "middle"
+
 	while(True):
 		print("<usage>")
-		print("start game: type 'g' | show score: type 's' | exit this script: type 'e'")
+		print("start game: type 'g' | show score: type 's' | change game mode: type 'm' | exit this script: type 'e'")
+		print("game mode: " + mode)
 		inStr = raw_input("please type key:")
 
 		if inStr == 'e' or inStr == 'E':
 			# exit script
 			print("thank you playing! bye :) ")
 			exit()
+
 		if inStr == 's' or inStr == 'S':
 			print("-----your score-----")
 			# TODO: show score list
-			continue
+
+		if inStr == 'm' or inStr == 'M':
+			print("please change game mode")
+			print("easy mode: type 'e' | middle mode: type 'm' | hard mode: type 'h'")
+			inMode = raw_input("please type game mode:")
+			if inMode == 'e' or inMode == 'E':
+				mode = "easy"
+			if inMode == 'm' or inMode == 'M':
+				mode = "middle"
+			if inMode == 'h' or inMode == 'H':
+				mode = "hard"
+			else:
+				print("sorry, your type is " + inMode + ". this is no matching..." )
 		if inStr == 'g' or inStr == 'G':
 			# start game
-			print(inStr) # debug script
-		else:
-			continue
+			print(inStr) # for debug
